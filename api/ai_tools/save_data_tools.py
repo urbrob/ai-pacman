9a46d64feb743c0f71eddcdff38f99837f01e178
urb.rob <urb.rob@o2.pl>
2020-04-25 16:17:02 +0200

    Add ai_tools

---
+++ api/ai_tools/save_data_tools.py
@@ -0,0 +1,15 @@
+
+
+
+def validate_if_data_is_valid_to_save(data) -> bool:
+    """EDITABLE. This function goal is to validate data if it is correct for database save."""
+    return True
+
+
+def format_saving_data_to_save(data):
+    """EDITABLE. This function goal is to prepare data to save it to database as you want."""
+    return data
+
+
+def save_data_to_database(data):
+    pass
---
+++ api/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, jsonify
+
+from api.ai_tools.save_data_tools import validate_if_data_is_valid_to_save, format_saving_data_to_save, save_data_to_database
+from api.ai_tools.predict_data_tools import validate_data_to_predict_move, format_data_to_predict_move, predicted_move_with_ai, format_predicted_move
+
+app = Flask(__name__, static_url_path="/static")
+app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
+mongo = PyMongo(app)
+
+@app.route("/api/save_move", methods=["POST"])
+def save_pacman_move_to_database():
+    if validate_if_data_is_valid_to_save(request.json):
+        formated_data = format_saving_data_to_save(request.json)
+        save_data_to_database(formated_data)
+        return {"status": "OK"}
+
+
+@app.route("/api/predict_move")
+def data():
+    if validate_data_to_predict_move(request.json):
+        formated_data = format_data_to_predict_move(request.json)
+        predicted_move = _predicted_move_with_ai(formated_data)
+        return format_predicted_move(predicted_move)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
