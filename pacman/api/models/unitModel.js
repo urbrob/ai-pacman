e08b41185609e3f06061d4193c29defc2d058226
mbarancewicz <44418145+mbarancewicz@users.noreply.github.com>
2020-06-06 12:59:57 +0200

    changes of unit and game models

--- pacman/api/models/unitModel.js
+++ pacman/api/models/unitModel.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var gameMap = require('./game_map');
+
 class Unit {
     constructor(r, c) {
         this.position = {
@@ -7,6 +9,32 @@ class Unit {
             col: c
         };
     }
+
+    move(direction) {
+        let previous = {
+            row: this.position.row,
+            col: this.position.col
+        };
+
+        if(direction === "right") this.position.col += 1;
+        else if(direction === "left") this.position.col -= 1;
+        else if(direction === "up") this.position.row -= 1;
+        else if(direction === "down") this.position.row += 1;
+
+        if(!this.validateMove(this.position)) {
+            this.position.row = previous.row;
+            this.position.col = previous.col
+        }
+
+        return this.position
+    };
+
+    validateMove(position) {
+        let field = gameMap.find(rowMap => rowMap.row_id === position.row).fields
+            .find(colMap => colMap.col_id === position.col).field_type;
+        return (field && field === "floor")
+    };
+
 }
 
 class Pacman extends Unit {}
